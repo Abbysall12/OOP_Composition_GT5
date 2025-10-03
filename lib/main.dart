3363163da514a51453d5873fa01bbe00c9364b66
Abraham <xdxdabrahamprololl10@gmail.com>
2025-10-03 19:08:24 +0800

    Complete OOP composition pattern implementation with AdventureParty, Warrior, and Rogue classes

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,13 @@
 
 import 'adventure_party.dart';
 import 'warrior.dart';
+import 'rogue.dart';
 
 void main() {
   print('=== RPG Adventure Party Demo ===\n');
   // instantiate the objects here.
 
-  // Parts created by members (Member 2 implemented Warrior)
+  // Parts created by members (Member 2 implemented Warrior, Member 3 implemented Rogue)
   var warrior = Warrior(
     name: 'DARK Knight',
     health: 10000,
@@ -17,13 +18,26 @@ void main() {
     weapon: 'YAMATOSWORD',
   );
 
+  var rogue = Rogue(
+    name: 'Shadow',
+    stealth: 90,
+    agility: 88,
+    weapon: 'Dual Daggers',
+  );
+
   // Container created by Member 1 with constructor injection
-  var party = AdventureParty(partyName: 'The Dragon Slayers', warrior: warrior);
+  var party = AdventureParty(
+    partyName: 'The Dragon Slayers', 
+    warrior: warrior,
+    rogue: rogue,
+  );
 
   // Display party status
   print('Party Status:');
   // show warrior status (accessing part's getter)
-  print('[Warrior] ' + warrior.status + '\n');
+  print('[Warrior] ' + warrior.status);
+  // show rogue status (accessing part's getter)
+  print('[Rogue] ' + rogue.status + '\n');
 
   // Demonstrate delegation (multiple container actions)
 
@@ -35,6 +49,9 @@ void main() {
 
   print('---Rest Sequence ---');
   party.takeRest();
+
+  print('---Stealth Mission ---');
+  party.performStealthMission();
 
   print('=== Adventure Complete! ===');
 }
